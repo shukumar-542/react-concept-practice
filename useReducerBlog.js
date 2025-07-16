8bbfd09bf215bc5999d949d81699c964f61fb203
shukumar-542 <shukumar542@gmail.com>
2025-07-16 09:46:18 +0600

    writing  a blog about useReducer

---
+++ useReducerBlog.js
@@ -0,0 +1,14 @@
+// 
+/**
+ What is useReducer?
+
+ useReducer is a react hook that allows you handle the complex state management in your component. in this hooks takes initialState and reducer function 
+ as a argument and return a array one is the current value another is dispatch function.
+ 
+ 
+
+
+
+
+
+ */
